79a1ed7d18a4baab1bf4d8e4dd81dd05ec9cdcc6
Waldyn Benbenek <wben1201@gmail.com>
2019-02-12 10:38:44 -0600

    Client side post done for now.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,18 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    $('.addJokeButton').on('click', addJoke)
 }
+function addJoke(){
+    let newJoke = {
+        whoseJoke: $('whoseJokeIn').val(),
+        jokeQuestion: $('questionIn').val(),
+        punchLine: $('punchlineIn').val()
+   }
+        $.ajax({
+            method: 'POST',
+            url: '/joke',
+            data: newJoke
+        })
+}
+
