82c3eabeba7a6574549d3c9818197faeb8eddb9d
Waldyn Benbenek <wben1201@gmail.com>
2019-02-12 10:56:54 -0600

    client side GET complete.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,16 +6,34 @@ function onReady() {
     console.log('DOM ready');
     $('.addJokeButton').on('click', addJoke)
 }
+function getJokes(){
+    $.ajax({
+        method: 'GET',
+        url: '/joke'
+    }).then(function(taco){
+        taco.forEach(function(ingredient){
+        $('#jokeTable').append(`
+        <tr>
+            <td>${ingredient.whoseJoke}</td>
+            <td>${ingredient.jokeQuestion}</td>
+            <td>${ingredient.punchLine}</td>
+        </tr>
+        `)
+    })
+})
+}
 function addJoke(){
     let newJoke = {
-        whoseJoke: $('whoseJokeIn').val(),
-        jokeQuestion: $('questionIn').val(),
-        punchLine: $('punchlineIn').val()
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine: $('#punchlineIn').val()
    }
         $.ajax({
             method: 'POST',
             url: '/joke',
             data: newJoke
+        }).then(function(){
+           console.log('post complete.')
         })
 }
 
